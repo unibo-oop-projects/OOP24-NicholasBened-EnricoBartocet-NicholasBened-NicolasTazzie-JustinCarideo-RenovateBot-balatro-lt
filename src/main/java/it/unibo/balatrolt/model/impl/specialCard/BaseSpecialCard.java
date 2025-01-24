5219773e2c1fbce7f3c03e78b698e6f638e79951
Nicolas Tazzieri <nicolas.tazzieri@studio.unibo.it>
2025-01-24 14:51:53 +0100

    Added equals and getHashCode methods

--- src/main/java/it/unibo/balatrolt/model/impl/specialCard/BaseSpecialCard.java
+++ src/main/java/it/unibo/balatrolt/model/impl/specialCard/BaseSpecialCard.java
@@ -37,4 +37,35 @@ public class BaseSpecialCard implements SpecialCard {
     public Optional<Modifier> getModifier() {
         return Optional.absent();
     }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result + ((description == null) ? 0 : description.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        BaseSpecialCard other = (BaseSpecialCard) obj;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        if (description == null) {
+            if (other.description != null)
+                return false;
+        } else if (!description.equals(other.description))
+            return false;
+        return true;
+    }
 }

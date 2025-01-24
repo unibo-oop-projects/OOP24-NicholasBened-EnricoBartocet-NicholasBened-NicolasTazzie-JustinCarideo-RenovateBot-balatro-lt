ea08e61fd3602c105aa52340834ecf6b36adc347
nicholas benedetti <nicholas.benedetti@studio.unibo.it>
2025-01-24 12:12:02 +0100

    MODIFIED : added addAll() method in Slot

--- src/main/java/it/unibo/balatrolt/model/api/Slot.java
+++ src/main/java/it/unibo/balatrolt/model/api/Slot.java
@@ -2,12 +2,32 @@ package it.unibo.balatrolt.model.api;
 
 import java.util.List;
 
+/**
+ * It models the concept of a Slot, where we
+ * can place any Card we want.
+ */
 public interface Slot {
 
+
+    /**
+     * @return the size of the Slot.
+     */
     int getSize();
 
+    /**
+     * @param card, to add in the Slot.
+     */
     void addCard(Card card);
 
-    List<Card> getCards();
+    /**
+     * @param cards, list containing the cards to add in the Slot.
+     *
+     * @throws
+     */
+    void addAll(List<? extends Card> cards);
 
+    /**
+     * @return the list of the cards contained in the Slot.
+     */
+    List<Card> getCards();
 }

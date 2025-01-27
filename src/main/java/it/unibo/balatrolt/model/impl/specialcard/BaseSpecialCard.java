cc1b69732c1705631ee4139d20ffe0187b1485c2
Nicolas Tazzieri <nicolas.tazzieri@studio.unibo.it>
2025-01-27 12:11:22 +0100

    fix: fixed joker equals
    Put final in hashCode and equals methods in BaseSpecialCard, so that two special cards are equal only if they have the same name and same description

--- src/main/java/it/unibo/balatrolt/model/impl/specialcard/BaseSpecialCard.java
+++ src/main/java/it/unibo/balatrolt/model/impl/specialcard/BaseSpecialCard.java
@@ -53,7 +53,7 @@ public class BaseSpecialCard implements SpecialCard {
     }
 
     @Override
-    public int hashCode() {
+    public final int hashCode() {
         final int prime = 31;
         int result = 1;
         result = prime * result + ((name == null) ? 0 : name.hashCode());
@@ -62,7 +62,7 @@ public class BaseSpecialCard implements SpecialCard {
     }
 
     @Override
-    public boolean equals(final Object obj) {
+    public final boolean equals(final Object obj) {
         if (this == obj) {
             return true;
         }

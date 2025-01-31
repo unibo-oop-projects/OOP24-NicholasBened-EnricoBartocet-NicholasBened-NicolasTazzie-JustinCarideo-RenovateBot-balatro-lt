5cc3d065118741380949bc11293b63bf61ff45b3
Enrico Bartocetti <enrico.bartocetti@studio.unibo.it>
2025-01-31 15:10:56 +0100

    refactor: Reversed the dependency for BlindConfiguration

--- src/main/java/it/unibo/balatrolt/model/impl/levels/BlindConfigurationImpl.java
+++ src/main/java/it/unibo/balatrolt/model/impl/levels/BlindConfigurationImpl.java
@@ -2,18 +2,20 @@ package it.unibo.balatrolt.model.impl.levels;
 
 import com.google.common.base.Preconditions;
 
+import it.unibo.balatrolt.model.api.levels.BlindConfiguration;
+
 /**
- * Represents the characteristics of a {@link it.unibo.balatrolt.model.api.levels.Blind}.
+ * A simple and immutable implementation of {@link BlindConfiguration}.
  * @author Bartocetti Enrico
  * @param id the id of the Blind
  * @param baseChip the minium of chips required to defeat the Blind
  * @param reward the reward which will be awarded to the player if he defeats the blind
  */
-public record BlindConfiguration(int id, int baseChip, int reward) {
+public record BlindConfigurationImpl(int id, int baseChip, int reward) implements BlindConfiguration {
     /**
      * Check that the chips required to defeat the blind and the reward aren't negative.
      */
-    public BlindConfiguration {
+    public BlindConfigurationImpl {
         Preconditions.checkArgument(baseChip >= 0, "The chips required to defeat the blind and the reward cannot be negative");
         Preconditions.checkArgument(reward >= 0, "The reward cannot be negative");
     }

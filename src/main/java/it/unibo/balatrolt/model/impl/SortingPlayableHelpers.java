31a6d8580e6fba067ba913fb59256b210aa791af
Justin Carideo <justin.carideo@studio.unibo.it>
2025-01-31 11:08:39 +0100

    fix: bug in SortingPlayableHelpers

--- src/main/java/it/unibo/balatrolt/model/impl/SortingPlayableHelpers.java
+++ src/main/java/it/unibo/balatrolt/model/impl/SortingPlayableHelpers.java
@@ -2,8 +2,6 @@ package it.unibo.balatrolt.model.impl;
 
 import java.util.List;
 
-import com.google.common.base.Preconditions;
-
 import it.unibo.balatrolt.model.api.PlayableCard;
 
 /**
@@ -21,7 +19,6 @@ public final class SortingPlayableHelpers {
      * @return the list ordered by Rank
      */
     public static List<PlayableCard> sortingByRank(final List<PlayableCard> hand) {
-        Preconditions.checkArgument(hand.isEmpty(), "Hand cannot be empty");
         return hand.stream().sorted((a, b) -> a.getRank().compareTo(b.getRank())).toList();
     }
 
@@ -30,7 +27,6 @@ public final class SortingPlayableHelpers {
      * @return the list ordered by Suit
      */
     public static List<PlayableCard> sortingBySuit(final List<PlayableCard> hand) {
-        Preconditions.checkArgument(hand.isEmpty(), "Hand cannot be empty");
         return sortingByRank(hand).stream().sorted((a, b) -> a.getSuit().compareTo(b.getSuit())).toList();
     }
 }

de76db447285dfdf142a0b2ca74b4abc59ff53dd
Nicholas Benedetti <nicholas.benedetti@studio.unibo.it>
2025-02-01 18:40:00 +0100

    feat: binding SlotGUI with controller

--- src/main/java/it/unibo/balatrolt/view/impl/SwingMainRound.java
+++ src/main/java/it/unibo/balatrolt/view/impl/SwingMainRound.java
@@ -4,7 +4,6 @@ import java.awt.BorderLayout;
 import java.io.IOException;
 import java.util.List;
 
-import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 /**
@@ -12,11 +11,9 @@ import javax.swing.JPanel;
  * parts that will form the final GUI.
  * @author Benedetti Nicholas
  */
-public class SwingMainRound extends JFrame {
+public class SwingMainRound extends JPanel {
 
     static final long serialVersionUID = 1L;
-    private static final int WIDTH_EXPAND = 500;
-    private static final int HEIGHT_EXPAND = 300;
     private SlotGUI slotPanel;
     private JPanel leftPanel;
 
@@ -34,17 +31,11 @@ public class SwingMainRound extends JFrame {
         //creating slots panel
         slotPanel = this.buildSlotPanel();
 
-        this.setVisible();
+        this.setVisible(true);
     }
 
     public void updateHand(List<String> hand) {
         slotPanel.updateHand(hand);
-    }
-
-    private void setVisible() {
-        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        this.setSize(WIDTH_EXPAND, HEIGHT_EXPAND);
-        this.setVisible(true);
     }
 
     private JPanel buildLeftPanel() {

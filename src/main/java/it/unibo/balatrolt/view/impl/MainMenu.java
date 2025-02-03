cfdb0f2e42a2914e58016593b2ae661538eb711c
Nicholas Benedetti <nicholas.benedetti@studio.unibo.it>
2025-02-03 15:26:00 +0100

    feat: made deck selector GUI

--- src/main/java/it/unibo/balatrolt/view/impl/MainMenu.java
+++ src/main/java/it/unibo/balatrolt/view/impl/MainMenu.java
@@ -19,39 +19,39 @@ import it.unibo.balatrolt.controller.api.MasterController;
 public class MainMenu extends JPanel {
     private Image image = new BufferedImage(1, 1, BufferedImage.TYPE_INT_ARGB);
 
-        public MainMenu(MasterController controller, String testo) {
-            setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
-            /**
-             * Setting the title.
-             */
-            final JLabel title = new JLabel("BALATRO");
-            title.setFont(new Font("Snap ITC", Font.BOLD, 70));
-            title.setForeground(Color.WHITE.brighter());
-            title.setAlignmentX(Component.CENTER_ALIGNMENT);
-            this.add(Box.createVerticalGlue());
-            this.add(title);
-            /**
-             * Setting the start button.
-             */
-            final JButton start = new JButton(testo);
-            start.setFont(new Font("Jokerman", Font.BOLD, 40));
-            start.setForeground(Color.WHITE.brighter());
-            start.setContentAreaFilled(false);
-            start.setBorder(null);
-            start.addActionListener(a -> controller.handleEvent(BalatroEvent.INIT_GAME, null));
-            start.setAlignmentX(Component.CENTER_ALIGNMENT);
-            this.add(Box.createVerticalGlue());
-            this.add(start);
-            this.add(Box.createVerticalGlue());
-            /**
-             * Setting the background.
-             */
-            try {
-                this.image = ImageIO.read(getClass().getResourceAsStream("/SFONDO_MAIN.png"));
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            this.setVisible(true);
+    public MainMenu(MasterController controller, String testo) {
+        setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+        /**
+         * Setting the title.
+         */
+        final JLabel title = new JLabel("BALATRO");
+        title.setFont(new Font("Snap ITC", Font.BOLD, 70));
+        title.setForeground(Color.WHITE.brighter());
+        title.setAlignmentX(Component.CENTER_ALIGNMENT);
+        this.add(Box.createVerticalGlue());
+        this.add(title);
+        /**
+         * Setting the start button.
+         */
+        final JButton start = new JButton(testo);
+        start.setFont(new Font("Jokerman", Font.BOLD, 40));
+        start.setForeground(Color.WHITE.brighter());
+        start.setContentAreaFilled(false);
+        start.setBorder(null);
+        start.addActionListener(a -> controller.handleEvent(BalatroEvent.INIT_GAME, null));
+        start.setAlignmentX(Component.CENTER_ALIGNMENT);
+        this.add(Box.createVerticalGlue());
+        this.add(start);
+        this.add(Box.createVerticalGlue());
+        /**
+         * Setting the background.
+         */
+        try {
+            this.image = ImageIO.read(getClass().getResourceAsStream("/SFONDO_MAIN.png"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        this.setVisible(true);
     }
 
     @Override

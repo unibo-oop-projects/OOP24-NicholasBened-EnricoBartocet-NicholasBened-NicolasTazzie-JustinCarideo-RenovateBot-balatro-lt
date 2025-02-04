b5146eb3dcba38614ded29e1b1e013b7fff699b0
Justin Carideo <justin.carideo@studio.unibo.it>
2025-02-04 17:55:50 +0100

    fix: left-down side of the gameplay GUI

--- src/main/java/it/unibo/balatrolt/view/impl/HandPanel.java
+++ src/main/java/it/unibo/balatrolt/view/impl/HandPanel.java
@@ -1,14 +1,11 @@
 package it.unibo.balatrolt.view.impl;
 
-import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.GridLayout;
 
 import javax.swing.BorderFactory;
-import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.SwingConstants;
 
 import it.unibo.balatrolt.controller.api.communication.AnteInfo;
 import it.unibo.balatrolt.controller.api.communication.BlindStats;
@@ -22,19 +19,14 @@ public class HandPanel extends JPanel {
     private final JLabel discardLabel = new JLabel();
     private final JLabel currencyLabel = new JLabel("$0");
     private final JLabel anteLabel = new JLabel("Ante: ");
+
     /**
      * Builds the GUI.
      * @param stats statistics of the current blind.
      */
     public HandPanel(final BlindStats stats) {
-        setLayout(new GridLayout(1, 2));
+        setLayout(new GridLayout(2, 1));
         updateHands(stats);
-        add(getButtonsPanel(), BorderLayout.WEST);
-        add(getStatsPanel(), BorderLayout.EAST);
-    }
-
-    private JPanel getStatsPanel() {
-        final JPanel statsPanel = new JPanel(new GridLayout(3, 1));
         final JPanel northPanel = new JPanel(new GridLayout(1, 2));
         handLabel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
         discardLabel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
@@ -43,23 +35,10 @@ public class HandPanel extends JPanel {
         this.currencyLabel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
         final JPanel southPanel = new JPanel(new GridLayout(1, 2));
         anteLabel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-        final JLabel roundPanel = new JLabel("Round: ");
-        roundPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
         southPanel.add(anteLabel);
-        southPanel.add(roundPanel);
-        statsPanel.add(northPanel);
-        statsPanel.add(currencyLabel);
-        statsPanel.add(southPanel);
-        return statsPanel;
-    }
-
-    private JPanel getButtonsPanel() {
-        final JPanel main = new JPanel(new GridLayout(2, 1));
-        final JButton buttonInfo = new JButton("Run Info");
-        final JButton buttonOptions = new JButton("Options");
-        main.add(buttonInfo, SwingConstants.CENTER);
-        main.add(buttonOptions, SwingConstants.CENTER);
-        return main;
+        southPanel.add(currencyLabel);
+        this.add(northPanel);
+        this.add(southPanel);
     }
 
     /**

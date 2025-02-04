f1927f9171aba2993ff77d07c94f0bb9c260da36
Enrico Bartocetti <enrico.bartocetti@studio.unibo.it>
2025-02-04 21:18:48 +0100

    fix: Fixed checkstyle for MasterController

--- src/main/java/it/unibo/balatrolt/controller/impl/MasterControllerImpl.java
+++ src/main/java/it/unibo/balatrolt/controller/impl/MasterControllerImpl.java
@@ -1,8 +1,5 @@
 package it.unibo.balatrolt.controller.impl;
 
-import static com.google.common.base.Preconditions.checkNotNull;
-import static com.google.common.base.Preconditions.checkState;
-
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -31,7 +28,7 @@ import it.unibo.balatrolt.view.api.View;
 /**
  * Implementation of {@link MasterController}.
  */
-public class MasterControllerImpl implements MasterController {
+public final class MasterControllerImpl implements MasterController {
 
     private static final int SHOP_SIZE = 3;
     private final Set<View> views = new HashSet<>();
@@ -52,7 +49,7 @@ public class MasterControllerImpl implements MasterController {
 
     @Override
     public void handleEvent(final BalatroEvent e, final Optional<?> data) {
-        checkState(this.nextEvents.contains(e), "Invalid event received: " + e.toString());
+        Preconditions.checkState(this.nextEvents.contains(e), "Invalid event received: " + e.toString());
         switch (e) {
             case MAIN_MENU -> views.forEach(View::showMainMenu);
             case INIT_GAME -> {
@@ -62,71 +59,86 @@ public class MasterControllerImpl implements MasterController {
             case CHOOSE_DECK -> {
                 setControllers(data);
                 this.levels.updateAnte();
-                views.forEach(v -> v.showSettings(this.levels.getCurrentBlindInfo(), this.levels.getCurrentBlindStats(), this.player.getSpecialCards(), this.player.getDeck(), this.levels.getNumAnte()));
-                views.forEach(v -> v.showAnte(this.levels.getCurrentAnte()));
-                views.forEach(v -> v.updateAnteInfo(this.levels.getCurrentAnte()));
+                this.views.forEach(v -> {
+                    v.showSettings(
+                        this.levels.getCurrentBlindInfo(),
+                        this.levels.getCurrentBlindStats(),
+                        this.player.getSpecialCards(),
+                        this.player.getDeck(),
+                        this.levels.getNumAnte()
+                    );
+                    v.showAnte(this.levels.getCurrentAnte());
+                    v.updateAnteInfo(this.levels.getCurrentAnte());
+                });
             }
             case CHOOSE_BLIND -> {
-                views.forEach(v -> v.showRound(this.levels.getHand()));
+                this.views.forEach(v -> v.showRound(this.levels.getHand()));
             }
-            case STAGE_CARDS -> {
-                recognizeCombination(data);
-            }
+            case STAGE_CARDS -> this.views.forEach(v -> v.updateCombinationStatus(recognizeCombination(data)));
             case DISCARD_CARDS -> {
                 this.levels.discardCards(checkPlayableCards(data));
-                views.forEach(v -> v.updateGameTable(this.levels.getHand(), this.levels.getCurrentBlindStats()));
-                views.forEach(v -> v.updateScore(this.levels.getCurrentBlindStats()));
+                this.views.forEach(v -> {
+                    v.updateGameTable(this.levels.getHand(), this.levels.getCurrentBlindStats());
+                    v.updateScore(this.levels.getCurrentBlindStats());
+                });
             }
             case PLAY_CARDS -> {
                 this.levels.playCards(checkPlayableCards(data), this.player.getPlayerStatus());
                 switch (this.levels.getRoundStatus()) {
                     case IN_GAME -> {
-                        views.forEach(v -> v.updateGameTable(this.levels.getHand(), this.levels.getCurrentBlindStats()));
+                        this.views.forEach(v -> v.updateGameTable(this.levels.getHand(), this.levels.getCurrentBlindStats()));
                     }
                     case BLIND_DEFEATED -> {
                         this.player.addCurrency(this.levels.getCurrentBlindInfo().reward());
-                        views.forEach(v -> v.updateCurrency(this.player.getCurrency()));
+                        this.views.forEach(v -> v.updateCurrency(this.player.getCurrency()));
                         if (this.levels.isOver()) {
-                            views.forEach(v -> v.showYouWon());
+                            this.views.forEach(View::showYouWon);
                         } else {
-                            views.forEach(v -> v.showBlindDefeated(this.levels.getCurrentBlindInfo(), this.levels.getCurrentBlindStats()));
+                            this.views.forEach(v -> {
+                                v.showBlindDefeated(this.levels.getCurrentBlindInfo(), this.levels.getCurrentBlindStats());
+                            });
                         }
                     }
-                    case BLIND_WON -> {
-                        views.forEach(v -> v.showGameOver());
-                    }
+                    case BLIND_WON -> this.views.forEach(View::showGameOver);
+                    default -> throw new IllegalStateException("Round Status unknown");
                 }
-                views.forEach(v -> v.updateScore(this.levels.getCurrentBlindStats()));
+                this.views.forEach(v -> v.updateScore(this.levels.getCurrentBlindStats()));
             }
             case OPEN_SHOP -> {
                 this.shop.openShop();
-                views.forEach(v -> {
+                this.views.forEach(v -> {
                     v.showShop();
                     v.updateShopCards(this.shop.getCards());
                 });
             }
             case BUY_CARD -> {
-                if (!buySpecialCard(data)) {
-                    views.forEach(v -> v.notifyErrror("Shop", "You don't have enough money"));
-                } else {
-                    views.forEach(v -> {
+                if (buySpecialCard(data)) {
+                    this.views.forEach(v -> {
                         v.updateShopCards(this.shop.getCards());
                         v.updateCurrency(this.player.getCurrency());
                         v.updateSpecialCards(this.player.getSpecialCards());
                     });
+                } else {
+                    this.views.forEach(v -> v.notifyErrror("Shop", "You don't have enough money"));
                 }
             }
             case SELL_CARD -> {
                 this.player.sellSpecialCard(checkSpecialCard(data));
-                views.forEach(v -> {
+                this.views.forEach(v -> {
                     v.updateSpecialCards(this.player.getSpecialCards());
                     v.updateCurrency(this.player.getCurrency());
                 });
             }
             case CLOSE_SHOP -> {
                 this.levels.updateAnte();
-                views.forEach(v -> {
-                    v.showSettings(this.levels.getCurrentBlindInfo(), this.levels.getCurrentBlindStats(), this.player.getSpecialCards(), this.player.getDeck(), this.levels.getNumAnte());
+                this.views.forEach(v -> {
+                    v.showSettings(
+                        this.levels.getCurrentBlindInfo(),
+                        this.levels.getCurrentBlindStats(),
+                        this.player.getSpecialCards(),
+                        this.player.getDeck(),
+                        this.levels.getNumAnte()
+                    );
                     v.showAnte(this.levels.getCurrentAnte());
                     v.updateAnteInfo(this.levels.getCurrentAnte());
                     v.updateCurrency(this.player.getCurrency());
@@ -139,18 +151,16 @@ public class MasterControllerImpl implements MasterController {
 
     @Override
     public void attachView(final View v) {
-        checkNotNull(v);
+        Preconditions.checkNotNull(v);
         views.add(v);
     }
 
     private boolean buySpecialCard(final Optional<?> data) {
-        Preconditions.checkArgument(data.isPresent(), "No card was received alongside the event");
-        Preconditions.checkArgument(data.get() instanceof SpecialCardInfo,
-                "The data received alongside the event isn't a SpecialCardInfo");
-        final var card = (SpecialCardInfo) data.get();
+        final var card = this.checkSpecialCard(data);
         if (this.shop.buyCard(card, this.player.getCurrency())
-                && this.shop.translateCard(card).isPresent()
-                && this.player.getSpecialCards().size() < this.player.getMaxSpecialCards()) {
+            && this.shop.translateCard(card).isPresent()
+            && this.player.getSpecialCards().size() < this.player.getMaxSpecialCards()
+        ) {
             this.player.addSpecialCard(this.shop.translateCard(card).get());
             this.player.spendCurrency(card.price());
             return true;
@@ -161,8 +171,7 @@ public class MasterControllerImpl implements MasterController {
 
     private void setControllers(final Optional<?> data) {
         Preconditions.checkArgument(data.isPresent(), "No deck was received alongside the event");
-        Preconditions.checkArgument(data.get() instanceof DeckInfo,
-                "The data received alongside the event isn't a DeckInfo");
+        Preconditions.checkArgument(data.get() instanceof DeckInfo, "The data received alongside the event isn't a DeckInfo");
         final var deck = deckTranslator.get((DeckInfo) data.get());
         this.levels = new LevelsControllerImpl(deck);
         this.player = new PlayerControllerImpl(deck);
@@ -175,19 +184,25 @@ public class MasterControllerImpl implements MasterController {
         return cards.stream().map(c -> (PlayableCardInfo) c).toList();
     }
 
-    private void recognizeCombination(final Optional<?> data) {
+    private CombinationInfo recognizeCombination(final Optional<?> data) {
         Preconditions.checkArgument(data.isPresent(), "No list was received alongside the event");
-        Preconditions.checkArgument(data.get() instanceof List,
-                "The data received alongside the event isn't a list");
+        Preconditions.checkArgument(data.get() instanceof List, "The data received alongside the event isn't a list");
         final var cards = (List<?>) data.get();
         final List<PlayableCard> list = this.levels.translatePlayableCard(cards.stream().map(c -> (PlayableCardInfo) c).toList());
         final Combination combination = new PlayedHandImpl(list).evaluateCombination();
-        this.views.forEach(v -> v.updateCombinationStatus(new CombinationInfo(combination.getCombinationType().toString(), combination.getBasePoints().basePoints(), combination.getMultiplier().multiplier())));
+        return new CombinationInfo(
+            combination.getCombinationType().toString(),
+            combination.getBasePoints().basePoints(),
+            combination.getMultiplier().multiplier()
+        );
     }
 
     private SpecialCardInfo checkSpecialCard(final Optional<?> data) {
         Preconditions.checkArgument(data.isPresent(), "No card was received alongside the event");
-        Preconditions.checkArgument(data.get() instanceof SpecialCardInfo, "The data received alongside the event isn't a SpecialCardInfo");
+        Preconditions.checkArgument(
+            data.get() instanceof SpecialCardInfo,
+            "The data received alongside the event isn't a SpecialCardInfo"
+        );
         return (SpecialCardInfo) data.get();
     }
 }

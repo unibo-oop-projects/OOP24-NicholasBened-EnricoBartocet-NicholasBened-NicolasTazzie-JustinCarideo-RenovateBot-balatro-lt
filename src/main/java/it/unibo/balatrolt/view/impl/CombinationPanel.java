d0de428bf6c2396e5681380f9744d26fc48e2ae7
Justin Carideo <justin.carideo@studio.unibo.it>
2025-02-06 09:22:28 +0100

    fix: checkstyle

--- src/main/java/it/unibo/balatrolt/view/impl/CombinationPanel.java
+++ src/main/java/it/unibo/balatrolt/view/impl/CombinationPanel.java
@@ -19,7 +19,7 @@ public final class CombinationPanel extends JPanel {
     private static final long serialVersionUID = 1L;
     private static final String FONT = "COPPER_BLACK";
     private static final int SCORE_DIM = 30;
-    private final FontFactory fontFactory = new FontFactory();
+    private static final FontFactory FONT_FACTORY = new FontFactory();
     private final JLabel combinationLabel;
     private final JLabel basePointsLabel;
     private final JLabel multiplierLabel;
@@ -43,12 +43,12 @@ public final class CombinationPanel extends JPanel {
         pointsTitle.setBackground(pointsTitle.getBackground().darker());
         pointsTitle.setText("Base Points");
         pointsTitle.setHorizontalAlignment(SwingConstants.CENTER);
-        pointsTitle.setFont(this.fontFactory.getFont(FONT, SCORE_DIM / 3 * 2, this));
+        pointsTitle.setFont(FONT_FACTORY.getFont(FONT, SCORE_DIM / 3 * 2, this));
         mainPanel.add(pointsTitle);
         final JLabel multiplierTitle = getMultiplierLabel();
         multiplierTitle.setBackground(multiplierTitle.getBackground().darker());
         multiplierTitle.setText("Multiplier");
-        multiplierTitle.setFont(this.fontFactory.getFont(FONT, SCORE_DIM / 3 * 2, this));
+        multiplierTitle.setFont(FONT_FACTORY.getFont(FONT, SCORE_DIM / 3 * 2, this));
         multiplierTitle.setHorizontalAlignment(SwingConstants.CENTER);
         mainPanel.add(multiplierTitle);
         mainPanel.add(basePointsLabel);
@@ -65,7 +65,7 @@ public final class CombinationPanel extends JPanel {
     private JLabel getCombinationLabel() {
         final JLabel combinationLabel = new JLabel();
         combinationLabel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-        combinationLabel.setFont(this.fontFactory.getFont(FONT, SCORE_DIM, this));
+        combinationLabel.setFont(FONT_FACTORY.getFont(FONT, SCORE_DIM, this));
         combinationLabel.setHorizontalAlignment(SwingConstants.CENTER);
         combinationLabel.setBackground(Color.DARK_GRAY.darker());
         combinationLabel.setForeground(Color.white);
@@ -81,7 +81,7 @@ public final class CombinationPanel extends JPanel {
         final JLabel pointsLabel = new JLabel();
         pointsLabel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
         pointsLabel.setHorizontalAlignment(SwingConstants.CENTER);
-        pointsLabel.setFont(this.fontFactory.getFont(FONT, SCORE_DIM, this));
+        pointsLabel.setFont(FONT_FACTORY.getFont(FONT, SCORE_DIM, this));
         pointsLabel.setBackground(Color.decode("#2274A5"));
         pointsLabel.setForeground(Color.white);
         pointsLabel.setOpaque(true);
@@ -95,7 +95,7 @@ public final class CombinationPanel extends JPanel {
     private JLabel getMultiplierLabel() {
         final JLabel multiplierLabel = new JLabel();
         multiplierLabel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-        multiplierLabel.setFont(this.fontFactory.getFont(FONT, SCORE_DIM, this));
+        multiplierLabel.setFont(FONT_FACTORY.getFont(FONT, SCORE_DIM, this));
         multiplierLabel.setHorizontalAlignment(SwingConstants.CENTER);
         multiplierLabel.setBackground(Color.decode("#c1121f"));
         multiplierLabel.setForeground(Color.white);

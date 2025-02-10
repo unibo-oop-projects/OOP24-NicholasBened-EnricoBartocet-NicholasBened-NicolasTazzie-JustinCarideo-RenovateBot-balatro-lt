61fd0b497fd34279bf317957bf8e3bda8d3f3c70
Enrico Bartocetti <enrico.bartocetti@studio.unibo.it>
2025-02-10 17:08:01 +0100

    fix: Suppresed SpotBugs warnings for serialization

--- src/main/java/it/unibo/balatrolt/view/impl/GameEnd.java
+++ src/main/java/it/unibo/balatrolt/view/impl/GameEnd.java
@@ -14,11 +14,21 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import it.unibo.balatrolt.controller.api.BalatroEvent;
 import it.unibo.balatrolt.controller.api.MasterController;
 /**
  * Shows the game over GUI.
  */
+@SuppressFBWarnings(
+    justification = """
+        Since we extend JPanel (which is Serializable), it's required to make the class Serializable,
+        otherwhise an exception will be thrown when serializing this class.
+        Anyway we are sure that we will never serialize this class, because if we want to save the game
+        we will only save the informations stored in the model, creating a new View when needed.
+        """,
+    value = "SE_BAD_FIELD"
+)
 public final class GameEnd extends JPanel {
     static final long serialVersionUID = 1L;
     private static final String FONT = "JOKERMAN";
--- src/main/java/it/unibo/balatrolt/view/impl/SlotPanel.java
+++ src/main/java/it/unibo/balatrolt/view/impl/SlotPanel.java
@@ -16,11 +16,22 @@ import javax.swing.JPanel;
 
 import com.google.common.base.Preconditions;
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+
 /**
  * Represent a Generic panel in which we can store any card.
  * we can also set the action to perform when the buttons are pressed.
  * @param <X> object the panel has to hold.
  */
+@SuppressFBWarnings(
+    justification = """
+        Since we extend JPanel (which is Serializable), it's required to make the class Serializable,
+        otherwhise an exception will be thrown when serializing this class.
+        Anyway we are sure that we will never serialize this class, because if we want to save the game
+        we will only save the informations stored in the model, creating a new View when needed.
+        """,
+    value =  "SE_BAD_FIELD"
+)
 public final class SlotPanel<X> extends JPanel {
     static final long serialVersionUID = 1L;
 
